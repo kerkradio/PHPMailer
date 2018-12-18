41d171c02478dd994a9013c3e9748a55f85b963b
kerkradio <43685475+kerkradio@users.noreply.github.com>
2018-12-18 10:39:01 +0200

    Add Afrikaans language file
    I am an Afrikaans first language speaker and well versed in English.
    
    Ek hoop Afrikaners kan dit goed gebruik!

---
+++ phpmailer.lang-af.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Afrikaans PHPMailer language file: refer to PHPMailer.php for definitive list.
+ * @package PHPMailer
+ * @author Sytze Visser <sytze@kerkradio.co.za>
+ */
+
+$PHPMAILER_LANG['authenticate']         = 'SMTP-fout: magtiging het misluk.';
+$PHPMAILER_LANG['connect_host']         = 'SMTP-fout: kon nie \'n konneksie met SMTP-host maak nie.';
+$PHPMAILER_LANG['data_not_accepted']    = 'SMTP-fout: data nie aanvaar nie.';
+$PHPMAILER_LANG['empty_message']        = 'ePos boodskap gedeelte is leeg';
+$PHPMAILER_LANG['encoding']             = 'Kodering is onbekend: ';
+$PHPMAILER_LANG['execute']              = 'Kon nie uitvoer nie: ';
+$PHPMAILER_LANG['file_access']          = 'Kry nie toegang tot lêer nie: ';
+$PHPMAILER_LANG['file_open']            = 'Lêer fout: kon lêer nie oop maak nie: ';
+$PHPMAILER_LANG['from_failed']          = 'Het volgende versender het misluk: ';
+$PHPMAILER_LANG['instantiate']          = 'Kon mailfunksie nie initialiseer nie.';
+$PHPMAILER_LANG['invalid_address']      = 'Ongeldig adres: ';
+$PHPMAILER_LANG['mailer_not_supported'] = ' mailer word nie ondersteund nie.';
+$PHPMAILER_LANG['provide_address']      = 'Ten minste een ontvanger word benodig.';
+$PHPMAILER_LANG['recipients_failed']    = 'SMTP-fout: die volgende ontvangers het misluk: ';
+$PHPMAILER_LANG['signing']              = 'Signeerfout: ';
+$PHPMAILER_LANG['smtp_connect_failed']  = 'SMTP Konneksie het misluk.';
+$PHPMAILER_LANG['smtp_error']           = 'SMTP-serverfout: ';
+$PHPMAILER_LANG['variable_set']         = 'Kan de volgende veranderlikes nie stel of terug stel nie: ';
+$PHPMAILER_LANG['extension_missing']    = 'Uitbreiding (extension) bestaan nie: ';
